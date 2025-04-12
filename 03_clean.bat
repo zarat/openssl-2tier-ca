8397529f0ff6718d2f674e767ebb9f27f2e2e522
Manuel Zarat <manuel@zarat.ml>
2025-04-12 03:31:04 +0200

    Update 03_clean.bat

--- 03_clean.bat
+++ 03_clean.bat
@@ -1,6 +1,10 @@
-cd ca\root
-del *.pem *.crt *.csr *.key
-cd ..\intermediate
-del *.pem *.crt *.csr *.key
-cd ..\..\
-del *.pem *.crt *.csr *.key
+cd ca
+cd root
+del *.pem *.crt *.csr *.key *.cer *.der
+cd ..
+cd intermediate
+del *.pem *.crt *.csr *.key *.cer *.der
+cd ..
+cd ..
+rmdir /Q /S output
+del *.sig
