7fbdc932f422159d35311cb20f8c548eb5e90f7c
Manuel Zarat <manuel@zarat.ml>
2025-04-12 03:21:01 +0200

    Update 00_make_root_ca.bat

--- 00_make_root_ca.bat
+++ 00_make_root_ca.bat
@@ -2,3 +2,7 @@
 
 REM ==== Root CA erstellen ====
 openssl req -newkey rsa:2048 -nodes -keyout ca\root\root-ca.key -x509 -days 3650 -out ca\root\root-ca.crt -config ca\root\openssl.cnf
+
+copy ca\root\root-ca.crt ca\root\root-ca.cer
+openssl x509 -in ca\root\root-ca.crt -outform DER -out ca\root\root-ca.der
+type ca\root\root-ca.key ca\root\root-ca.crt > ca\root\root-ca.pem
