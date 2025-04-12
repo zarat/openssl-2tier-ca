5ff71c4dbdaa10564892df9d5cc5357454896d25
Manuel Zarat <manuel@zarat.ml>
2025-04-12 03:22:15 +0200

    Update 01_make_intermediate_ca.bat

--- 01_make_intermediate_ca.bat
+++ 01_make_intermediate_ca.bat
@@ -7,3 +7,6 @@ openssl req -new -key ca\intermediate\intermediate-ca.key -out ca\intermediate\i
 REM Intermediate CA signieren
 openssl x509 -req -in ca\intermediate\intermediate-ca.csr -CA ca\root\root-ca.crt -CAkey ca\root\root-ca.key -set_serial 100 -days 3650 -out ca\intermediate\intermediate-ca.crt -extensions v3_ca -extfile ca\intermediate\openssl.cnf
 
+copy ca\intermediate\intermediate-ca.crt ca\intermediate\intermediate-ca.cer
+openssl x509 -in ca\intermediate\intermediate-ca.crt -outform DER -out ca\intermediate\intermediate-ca.der
+type ca\intermediate\intermediate-ca.key ca\intermediate\intermediate-ca.crt > ca\intermediate\intermediate-ca.pem
